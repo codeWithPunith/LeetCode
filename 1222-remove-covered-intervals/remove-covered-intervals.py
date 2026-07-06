aed836cae9c40c2f8f63e2594b96568db8d65f84
Punithraj R <codewithpunith@gmail.com>
2026-07-06 23:06:19 +0530

    Time: 2 ms (83.42%) | Memory: 19.7 MB (47.88%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.py
+++ 1222-remove-covered-intervals/remove-covered-intervals.py
@@ -1,7 +1,25 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        """"the solution is easy when u sort the intervals it makes sure that every start will be greater or equal to the previous start ie you will always get a greater start interval, so all u need to check is if the current end is greater than the previous end if its greater then there is no merge if not the interval is merged """
+        intervals.sort(key = lambda x : (x[0],-x[1]))
+        maxend=0
+        res=0
+        for s,e in intervals:
+            if e>maxend:
+                maxend =e
+                res+=1
+        return res
+       
+        
+        
+        
+        
+        
+        
+        """
         #bruteforce 
         res=0
+        arr=[]
         for i,interval in enumerate(intervals):
             start=interval[0]
             end = interval[1]
@@ -13,4 +31,9 @@ class Solution:
                     iscovered= True
             if not iscovered:
                 res+=1
+                arr.append([start,end])
+        print(arr)
         return res
+        """
+
+        
