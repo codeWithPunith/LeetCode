45a9456be8a67c10bb1732e4645578518f20c691
Punithraj R <codewithpunith@gmail.com>
2026-08-10 22:20:37 +0530

    Time: 12 ms (26.79%) | Memory: 13.8 MB (55.43%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -4,21 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        cache ={}
         count = Counter(nums)
-        nums =sorted(count.keys())
-        def helper(i):
-            if i in cache:
-                return cache[i]
-            if i>=len(nums):
-                return 0
+        nums = sorted(count.keys())
+        if len(nums)==0:
+            return 0
+        if len(nums)==1:
+            return nums[0]*count[nums[0]]
+        dp=[0]*len(nums)
+        dp[0]=nums[0]*count[nums[0]]
+        if nums[0]+1==nums[1]:
+            dp[1] = max(nums[1]*count[nums[1]],dp[0])
+        else:
+            dp[1] = max(nums[1]*count[nums[1]]+dp[0],dp[0])
+        for i in range(2,len(nums)):
             include = nums[i]*count[nums[i]]
-            if i+1<len(nums) and nums[i]+1==nums[i+1]:
-                include = include + helper(i+2)
-            else :
-                include = include + helper(i+1)
-            notInclude = helper(i+1)
-            cache[i]=max(include,notInclude)
-            return max(include,notInclude)
+            if nums[i-1]!=nums[i]-1:
+                include = include+dp[i-1]
+            else:
+                include = include+dp[i-2]
+            dp[i]=max(include,dp[i-1])
+        return dp[-1]
             
-        return helper(0)
+
